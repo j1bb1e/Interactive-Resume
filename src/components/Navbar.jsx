d6e6523ba83abf85512f8474cee6bc79e68301c1
Julia <juliabrand@usf.edu>
2024-10-21 20:23:33 -0400

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import logo from "../assets/logo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/julia-brand-430628293/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/j1bb1e" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+

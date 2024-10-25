7bbd1ccafdc2bbd4c1f88305ae4bdffc641ab2ed
j1bb1e <jbrandabot1@gmail.com>
2024-10-25 14:21:28 -0400

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
+  //address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+1 (609)582-0945 ",
   email: "me@example.com",
 };
